d08936663b5d80c006e11295587696e96a46e9e7
baylee-p <b_peterson4@mail.fhsu.edu>
2025-03-27 18:41:10 -0500

    Add CORS headers

--- api/authors/index.php
+++ api/authors/index.php
@@ -3,9 +3,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    $method = $_SERVER['REQUEST_METHOD'];
 
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+        exit();
+    }
+    
 include_once "../Database.php";
 include_once "../Author.php";
 
--- api/quotes/index.php
+++ api/quotes/index.php
@@ -4,9 +4,15 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Headers
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    $method = $_SERVER['REQUEST_METHOD'];
 
+    if ($method === 'OPTIONS') {
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
+        exit();
+    }
 // Include Files
 include_once "../Database.php";
 include_once "../Quote.php";
