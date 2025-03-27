cf73e453fe394b764cc5578cdc839b465328a8fb
baylee-p <b_peterson4@mail.fhsu.edu>
2025-03-26 20:45:03 -0500

    Db

--- api/Database.php
+++ api/Database.php
@@ -1,25 +1,27 @@
 <?php
 class Database {
-    private $host = "localhost";
-    private $db_name = "quotesdb";
-    private $username = "root";
-    private $password = "";
+    private $host = "dpg-cviapnfnoe9s739sm7bg-a";
+    private $db_name = "quotesdb_km3l";
+    private $username = "quotesdb_km3l_user";
+    private $password = "ZE9jj9RNQKKYoVoNaCJbcMP42xerVtGt";
+    private $port = "5432";
     public $conn;
 
-public function getConnection() {
-    $this->conn = null;
-    try {
-        $this->conn = new PDO(
-            "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
-            $this->username,
-            $this->password
-        );
-        $this->conn->exec("set names utf8");
-    } catch (PDOException $exception) {
-        echo "Connection error: " . $exception->getMessage();
-    }
+    public function getConnection() {
+        $this->conn = null;
 
-    return $this->conn;
+        try {
+            $this->conn = new PDO(
+                "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};",
+                $this->username,
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
     }
 }
 ?>
